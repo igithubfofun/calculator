9198b741c62ba97c70757d753d52636769a9f464
Metel Patel <metelpatel@gmail.com>
2015-12-11 14:33:16 -0600

    working on shuffle colors array

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,24 @@ $(function(){
   $('.numbers').not('#equals').on('click', function(){
     var selected = $(this).text();
     var amount = $('#amount').val();
+    var colors = ['#1abc9c', '#2ecc71']
     $('#amount').val(amount += selected);
+
+    function shuffle(array) {
+    var counter = array.length, temp, index;
+      while (counter > 0) {
+          index = Math.floor(Math.random() * counter);
+          counter--;
+          temp = array[counter];
+          array[counter] = array[index];
+          array[index] = temp;
+          }
+
+          return array;
+    }
+    console.log(shuffle(unshift(colors)));
+
+    $(this).css('background-color', shuffle(colors));
   })
 
   $('#clear').on('click', function(){
