c18613fd794cfe8d3316ad0bc8c086863ed0bd76
Metel Patel <metelpatel@gmail.com>
2015-12-11 15:18:37 -0600

    completed shuffled colors array

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,23 @@
 
 $(function(){
 
+    function shuffle(array) {
+    var counter = array.length, temp, index;
+      while (counter > 0) {
+          index = Math.floor(Math.random() * counter);
+          counter--;
+          temp = array[counter];
+          array[counter] = array[index];
+          array[index] = temp;
+      }
+
+      return array;
+    }
+    var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#16a085', '#27ae60', '#2980b9', '#8e44ad',
+    '#f1c40f', '#e67e22', '#e74c3c', '#ecf0f1', '#95a5a6', '#f39c12', '#d35400', '#c0392b', '#bdc3c7'];
+    var shuffledColor;
+
+
 
   $('#equals').on('click', function(e){
     var calculation = $('#amount').val();
@@ -15,30 +32,24 @@ $(function(){
 
     $('#amount').val('');
 
+    $('.numbers').css('background-color', '#34495e');
+
+
+
   })
 
   $('.numbers').not('#equals').on('click', function(){
     var selected = $(this).text();
     var amount = $('#amount').val();
-    var colors = ['#1abc9c', '#2ecc71']
+
     $('#amount').val(amount += selected);
+    shuffledColor = shuffle(colors).shift();
 
-    function shuffle(array) {
-    var counter = array.length, temp, index;
-      while (counter > 0) {
-          index = Math.floor(Math.random() * counter);
-          counter--;
-          temp = array[counter];
-          array[counter] = array[index];
-          array[index] = temp;
-          }
+    $(this).css('background-color', shuffledColor);
 
-          return array;
-    }
-    console.log(shuffle(unshift(colors)));
-
-    $(this).css('background-color', shuffle(colors));
   })
+
+
 
   $('#clear').on('click', function(){
     $('#amount').val('');
