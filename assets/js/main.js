1c8bcf9035f1ec099798e54f7873fd6cf56d3987
Samuel Ryan Waits <ryanwaits@Samuels-MacBook-Air.local>
2015-12-15 12:01:38 -0600

    localstorage object storing input // data does not persist on log after refresh

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,11 +13,16 @@ $(function(){
     return array;
   }
 
+  function storageLog(){
+
+  }
+
   var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#16a085', '#27ae60', '#2980b9', '#8e44ad',
   '#f1c40f', '#e67e22', '#e74c3c', '#ecf0f1', '#95a5a6', '#f39c12', '#d35400', '#c0392b', '#bdc3c7'];
   var shuffledColor;
 
-  var count = 1;
+  var count = 0;
+  var keyCount = 0;
 
   $('#equals').on('click', function(e){
     var calculation = $('#amount').val();
@@ -25,10 +30,11 @@ $(function(){
       var result = eval(calculation).toPrecision(2);
       $('#amount').val(result);
 
-      $('.logs').append('<p>' + count + ' ) ' + calculation + ' = ' + result + '</p>');
 
       var logCount = $('p.logs').length;
       count += logCount;
+
+      keyCount += 1;
 
       $('#amount').val('');
 
@@ -36,6 +42,11 @@ $(function(){
     } else {
       alert('nah');
     }
+
+    localStorage.setItem('log' + keyCount, (calculation) + ' = ' + result);
+    var log = localStorage.getItem('log' + keyCount);
+    $('.logs').append('<p>' + log + '</p>');
+
 
   })
 
@@ -59,14 +70,17 @@ $(function(){
       var amount = $('#amount').val();
       var result = eval(amount);
 
-      $('.logs').append('<p>' + count + ' ) ' + amount + ' = ' + result.toPrecision(2) + '</p>');
 
       var logCount = $('p.logs').length;
       count += logCount;
 
       $('#amount').val('');
-       $('.numbers').css('background-color', '#34495e');
+      $('.numbers').css('background-color', '#34495e');
     }
+
+    localStorage.setItem('log' + count, (amount) + ' = ' + result.toPrecision(2) );
+    var log = localStorage.getItem('log' + count);
+    $('.logs').append('<p>' + log + '</p>');
   })
 
 })
